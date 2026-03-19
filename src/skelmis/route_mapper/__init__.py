b7900d5717261ebd259afc11410a7e783d7a1c1d
skelmis <skelmis.craft@gmail.com>
2026-03-19 23:02:36 +1300

    chore: bump version

--- src/skelmis/route_mapper/__init__.py
+++ src/skelmis/route_mapper/__init__.py
@@ -1,6 +1,6 @@
 from .ast import file_to_api_class
 from .transform import transform_ast_to_routes
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = ["file_to_api_class", "transform_ast_to_routes", "ast", "transform", "rules"]
